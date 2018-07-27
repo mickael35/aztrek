592f65bba9080311ad8e0312ac88bd6f135617a6
mherve <mickael.herve77@gmail.com>
2018-07-27 15:24:33 +0200

    depart

---
+++ index.php
@@ -0,0 +1,222 @@
+<!doctype html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="description" content="...">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>AZTREK</title>
+  <link rel="shortcut icon" href="favicon.ico">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp"
+    crossorigin="anonymous">
+  <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+  <link rel="stylesheet" href="css/jquery.sidr.light.min.css">
+  <link rel="stylesheet" href="css/owl.carousel.min.css">
+  <link rel="stylesheet" href="css/styles.css">
+
+</head>
+
+<body class="page-home">
+  <header class="page-header">
+    <div class="page-header-inner container">
+
+      <a class="burger" href="#sidr-main">
+        <i class="fa fa-bars" aria-hidden="true"></i>Menu
+      </a>
+
+      <div class="header-logo">
+        <div class="logo">
+          <a href="index.html" title="Accueil">
+            <img src="images/logo.png" alt="Logo">
+          </a>
+        </div>
+        <h2 class="site-title">Eclipse</h2>
+      </div>
+
+      <div class="header-nav">
+
+        <nav class="main-nav">
+          <ul class="container">
+            <li>
+              <a href="#">Nos sejours</a>
+            </li>
+            <li>
+              <a href="#">Qui sommes nous ?</a>
+            </li>
+            <li>
+              <a href="#">Mon compte</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+        </nav>
+        <h2>Un nouveau monde à chaques pas</h2>
+
+        <form class="search-form" action="#" method="get">
+          <input type="text" name="keywords" value="" placeholder="Rechercher">
+          <button type="submit" name="submit-btn">
+            <i class="fa fa-search" aria-hidden="true"></i>Valider</button>
+        </form>
+
+      </div>
+
+    </div>
+  </header>
+
+  <main>
+    <section class="owl-carousel">
+
+      <article class="item">
+        <img src="./images/guatemala_slider.png" alt="">
+      </article>
+      <article class="item">
+        <img src="./images/guatemala_slider.png" alt="">
+      </article>
+
+
+
+    </section>
+
+    <section class="sejour">
+      <div class="sejour-home-pres">
+        <a href="#" class="sejour-home-visuel">
+          <img src="images/photo_costa.png" alt="">
+        </a>
+        <article class="sejour-home-infos">
+          <h3 class="title-sejour">Costa ricas </h3>
+
+
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+            aliqua...
+          </p>
+
+        </article>
+      </div>
+      <nav class="sejour-home-ctc">
+        <a href="#" class="detail-sejour">Voir Plus</a>
+
+      </nav>
+    </section>
+    <section class="sejour">
+      <div class="sejour-home-pres">
+        <a href="#" class="sejour-home-visuel">
+          <img src="images/photo_guatemala.png" alt="">
+        </a>
+        <article class="sejour-home-infos">
+          <h3 class="title-sejour">guatemala </h3>
+
+
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+            aliqua...
+          </p>
+
+        </article>
+      </div>
+      <nav class="sejour-home-ctc">
+        <a href="#" class="detail-sejour">Voir Plus</a>
+
+      </nav>
+    </section>
+    <section class="sejour">
+      <div class="sejour-home-pres">
+        <a href="#" class="sejour-home-visuel">
+          <img src="images/photo_honduras.png" alt="">
+        </a>
+        <article class="sejour-home-infos">
+          <h3 class="title-sejour">honduras </h3>
+
+
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+            aliqua...
+          </p>
+
+        </article>
+      </div>
+      <nav class="sejour-home-ctc">
+        <a href="#" class="detail-sejour">Voir Plus</a>
+
+      </nav>
+    </section>
+    <section class="sejour">
+      <div class="sejour-home-pres">
+        <a href="#" class="sejour-home-visuel">
+          <img src="images/photo_mexico.png" alt="">
+        </a>
+        <article class="sejour-home-infos">
+          <h3 class="title-sejour">mexique </h3>
+
+
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+            aliqua...
+          </p>
+
+        </article>
+      </div>
+      <nav class="sejour-home-ctc">
+        <a href="#" class="detail-sejour">Voir Plus</a>
+
+      </nav>
+    </section>
+    <section class="sejour">
+      <div class="sejour-home-pres">
+        <a href="#" class="sejour-home-visuel">
+          <img src="images/photo_salvador.png" alt="">
+        </a>
+        <article class="sejour-home-infos">
+          <h3 class="title-sejour">salvador </h3>
+
+
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+            aliqua...
+          </p>
+
+        </article>
+      </div>
+      <nav class="sejour-home-ctc">
+        <a href="#" class="detail-sejour">Voir Plus</a>
+
+      </nav>
+    </section>
+
+  </main>
+
+  <footer>
+
+
+
+
+    <div class="container footer">
+      <nav class="footer-social-link">
+        <a href="https://www.facebook.com/" title="suivez nous sur facebook">
+          <img src="images/facebook.png">
+        </a>
+        <a href="https://www.instagram.com/" title="suivez nous sur instagram">
+          <img src="images/instagram.png">
+        </a>
+        <a href="https://twitter.com/" title="suivez nous sur twitter">
+          <img src="images/photo_twitter.png">
+        </a>
+        <a href="https://www.youtube.com/" title="suivez nous sur youtube">
+          <img src="images/photo_youtube.png">
+        </a>
+    </div>
+    <div class="logo_footer">
+      <img src="images/logo.png">
+      <p>Copyright</p>
+      <p>CGV</p>
+      <p>Mentions légales</p>
+
+    </div>
+
+  </footer>
+
+  <script src="js/jquery-3.1.1.min.js"></script>
+  <script src="js/jquery.sidr.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/scripts.js"></script>
+
+</body>
+
+</html>
